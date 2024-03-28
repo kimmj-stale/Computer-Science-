444f4557424f4bfa5c797bb59bf742b727df322e
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-03-28 09:38:46 +0900

    Update _07_circular_linkedlist.cpp

--- CS/데구/_07_circular_linkedlist.cpp
+++ CS/데구/_07_circular_linkedlist.cpp
@@ -90,24 +90,3 @@ int delete(linkedList* L, listNode* p) {
     return TRUE;
 }
 */
-
-/**
- * polynomial (다항식)
- * 각 항마다 계수와 지수로 형성되어 있으므로 계수 ai , 지수 xi로 구성된 순서쌍 <ai , xi>로 순서대로 나열할 수 있다.
- * 희소 다항식 (항과 항간의 차수가 많이 차이나는 경우)
- *  >>  array를 만들어 계수가 0이 아닌 항의 차수와 계수를 저장
-*/
-
-/*
-//polynomial node 형성
-typedef struct PolyNode {
-    float coef;                 >>      항의 갯수
-    int exp;                    >>      지수의 차수
-    struct Node* link;          >>      다음 항의 node
-}Node;
-
-typedef struct PolyList {
-    Node* head;   
-    Node* last;             >>      브루트 포스 알고리즘처럼 다항식의 첫 번째 항부터 마지막 항까지 모두 훑으면서 탐색
-}polyList;
-*/

9fc3950b44c3073a3f7c16f2f03b39b246f3b2f9
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-03-29 12:56:13 +0900

    Add files via upload

---
+++ 객체/실습/240328.cpp
@@ -0,0 +1,42 @@
+/*1
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    while (1) {
+        cout << "Enter the size (1 to 10): ";
+        cin >> n;
+        if (n >= 1 && n <= 10) {
+            break;
+        }
+    }
+
+    int num;
+    int array[10] = {0};
+    cout << "Enter " <<  n << " integer(s): ";
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        array[i] = num;
+    }
+    cout << "Integer(s) in reversed order:";
+    for (int i = n - 1; i >= 0; i--) {
+        cout << ' ' << array[i];
+    }
+}
+*/
+
+/*2
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5];
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] * 2 << ' ';
+    }
+    cout << '\n';
+}
+*/

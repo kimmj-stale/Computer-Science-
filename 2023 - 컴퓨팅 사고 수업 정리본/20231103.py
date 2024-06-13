9b1b7ed6517ec5209e3ac16d574ce87b1e0977a4
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-06-13 16:44:07 +0900

    Update 20231103.py

--- 2023 - 컴퓨팅 사고 수업 정리본/20231103.py
+++ 2023 - 컴퓨팅 사고 수업 정리본/20231103.py
@@ -1,3 +1,4 @@
+/*
 weight = float(input('수하물의 무게를 입력해주세요 : '))
 
 a = (weight - 20)*4000
@@ -8,3 +9,4 @@ if weight >= 20:
     print('무게가 20kg이 초과되어 추가비용 %d,%d 원이 추가 발생합니다.' %(plus,minus))
 else:
     print('수하물의 무게는' , weight , 'kg 입니다.')
+*/

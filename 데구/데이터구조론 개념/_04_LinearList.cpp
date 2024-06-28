c1d3ad9daefcc14eaaf48db4ece738d19440af72
Trifolium991119 <150104295+kimmj-stale@users.noreply.github.com>
2024-06-29 08:56:22 +0900

    Update and rename _04_linearList.cpp to _04_LinearList.cpp

---
+++ 데구/데이터구조론 개념/_04_LinearList.cpp
@@ -0,0 +1,61 @@
+/**
+ * linear list 구조체
+*/
+
+/* 구조체 선언과 기본 연산
+linear list 구조체
+typedef int element;
+typedef struct {
+    //선언이 필요한 자료형들
+}ArrayList;
+
+리스트 초기화
+void init(ArrayList *L) {
+    L -> length = 0;
+}
+
+isEmpty 연산
+int isEmpty(ArrayList* L) {
+    return L -> length == 0;
+}
+
+isFull 연산
+int isFull(ArrayList* L) {
+    return L -> length == (max_size 값);
+}
+*/
+
+/* insert 연산
+void insert(ArrayListType *L, int pos, element item){
+    int i;
+    if(isFull(L))
+        error(“List is Full!”)
+    else if(pos < 0 || pos > L -> length)
+        error(“Index Error”)
+    else{
+        for(i = L -> length - 1; i >= pos; i--)
+            L -> list[i + 1] = L -> list[i];
+        L -> list[pos] = item;
+        L -> length++;
+    }
+}
+*/
+
+/* delete 연산
+element delete(ArrayListType *L, int pos){
+    int i;
+    element item;
+    if(isEmpty(L))
+        error(“List is Empty!”)
+    else if(pos < 0 || pos >= L -> length)
+        error(“Index Error”)
+    else{
+        item = L -> list[pos];
+        for(i = pos; i < L -> length - 1; i++) {
+            L -> list[i] = L -> list[i + 1];
+        }
+        L -> length--;
+        return item;
+    }
+}
+*/
